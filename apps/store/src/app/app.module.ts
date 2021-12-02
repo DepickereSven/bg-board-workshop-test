8355f9532be152a59074bddaa9538407d9a66924
Depickere Sven <sven.depickere@hotmail.com>
2021-12-02 12:03:35 +0100

    Lab 6: Add routing lib

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -3,19 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { MatCardModule } from "@angular/material/card";
-import { StoreUiSharedModule } from "@bg-board/store/ui-shared";
+import { MatCardModule } from '@angular/material/card';
+import { StoreUiSharedModule } from '@bg-board/store/ui-shared';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     StoreUiSharedModule,
-    RouterModule.forRoot([], {initialNavigation: 'enabledBlocking'}),
+    RouterModule.forRoot(
+      [
+        {
+          path: 'store-feature-game-detail',
+          loadChildren: () =>
+            import('@bg-board/store/feature-game-detail').then(
+              (module) => module.StoreFeatureGameDetailModule
+            ),
+        },
+      ],
+      { initialNavigation: 'enabledBlocking' }
+    ),
     MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
