e11520a0938c00dbfff1cf84e0dc2a98c5e42b3e
Depickere Sven <sven.depickere@hotmail.com>
2021-12-02 16:27:05 +0100

    Lab 11: Update tests

--- apps/store-ui-shared-e2e/src/integration/header/header.component.spec.ts
+++ apps/store-ui-shared-e2e/src/integration/header/header.component.spec.ts
@@ -1,6 +1,13 @@
 describe('store-ui-shared', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=headercomponent--primary&args=title;'));
+  beforeEach(() => cy.visit(
+    '/iframe.html?id=headercomponent--primary&args=title:BoardGameHoard;'
+  ));
   it('should render the component', () => {
     cy.get('bg-board-header').should('exist');
+  });
+
+
+  it('should show the title', () => {
+    cy.get('bg-board-header').contains('BoardGameHoard');
   });
 });
